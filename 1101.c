9e8048979e417c2d059f8ec7982ec8a0708882b5
Thiago Henrique D. Botelho <thenrique2012@gmail.com>
2018-05-28 19:56:47 -0300

    URI Problems solution in C

--- 1101.c
+++ 1101.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 int main() {
-	int i, n, m, soma=0;	
-	
-	do {
-		scanf("%d %d", &n, &m);;
-		soma = 0;
-		
-		if (n <= 0 || m <= 0) { 
-			break;
-		}
-		
-		if (n > m) {	
-			for(i = m; i <= n; i++) {
-				printf("%d ",i);
-				soma += i;
-			}
-			printf("Sum=%d\n",soma);
-		} else  {
-			for(i = n; i <= m; i++) {
-				printf("%d ",i);
-				soma += i;
-			}
-			printf("Sum=%d\n",soma);
-		}		
-	}  while ((n > 0 && n != 0) && (m > 0 && m != 0)) ;			
-} 
-	
+    int i, n, m, soma = 0;
+
+    do {
+        scanf("%d %d", &n, &m);
+        ;
+        soma = 0;
+
+        if (n <= 0 || m <= 0) {
+            break;
+        }
+
+        if (n > m) {
+            for (i = m; i <= n; i++) {
+                printf("%d ", i);
+                soma += i;
+            }
+            printf("Sum=%d\n", soma);
+        } else {
+            for (i = n; i <= m; i++) {
+                printf("%d ", i);
+                soma += i;
+            }
+            printf("Sum=%d\n", soma);
+        }
+    } while ((n > 0 && n != 0) && (m > 0 && m != 0));
+}
